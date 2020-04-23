fdefcec1e814fb1c8e8502500d756f01a377999c
Dan <danreader0@gmail.com>
2020-04-23 16:01:00 -0400

    fix database connectionString

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true
   }
 });
@@ -46,5 +46,5 @@ app.post('/imageurl', (req, res) => {
 })
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('app is running on port ${process.env.PORT}');
+  console.log(`app is running on port ${process.env.PORT}`);
 })
